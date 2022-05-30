8ef1f6188708b014726870aee84432bf3811d550
tienthanhvimass <tienthanh.vimass@gmail.com>
2022-05-30 23:17:12 +0700

    update

---
+++ app/src/main/java/com/example/textdetector/User.java
@@ -0,0 +1,16 @@
+package com.example.textdetector;
+
+public class User {
+    public String date_of_birth;
+    public String full_name;
+    public String nickname;
+
+    public User(String dateOfBirth, String fullName) {
+        // ...
+    }
+
+    public User(String dateOfBirth, String fullName, String nickname) {
+        // ...
+    }
+
+}
